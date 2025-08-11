b17934db6432f62e00fac6254e8c0ef6501c31d7
quangthangit <thangtq.it@gmail.com>
2025-08-11 14:18:49 +0700

    fix: Add doman access chatsocket

--- src/main/java/com/EdumentumBackend/EdumentumBackend/config/WebSocketConfig.java
+++ src/main/java/com/EdumentumBackend/EdumentumBackend/config/WebSocketConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -13,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .setAllowedOriginPatterns("http://localhost:3000")
+                .setAllowedOriginPatterns(String.valueOf(List.of("http://localhost:3000","https://edumentum.vercel.app")))
                 .withSockJS();
     }
 
