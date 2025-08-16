ff19af6df01d001c3210543e39976e3296aedd65
Quang Thang <94356792+quangthangit@users.noreply.github.com>
2025-08-16 11:54:52 +0700

    fix: update application-test.properties for H2 test database

--- src/test/java/com/EdumentumBackend/EdumentumBackend/EdumentumBackendApplicationTests.java
+++ src/test/java/com/EdumentumBackend/EdumentumBackend/EdumentumBackendApplicationTests.java
@@ -3,6 +3,7 @@ package com.EdumentumBackend.EdumentumBackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class EdumentumBackendApplicationTests {
 
