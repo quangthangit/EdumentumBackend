ad0f5b315b0a39de0d0936b386ffeae6408d05a2
quangthangit <thangtq.it@gmail.com>
2025-08-20 15:07:13 +0700

    fix: google-services.json

--- src/main/java/com/EdumentumBackend/EdumentumBackend/config/FirebaseConfig.java
+++ src/main/java/com/EdumentumBackend/EdumentumBackend/config/FirebaseConfig.java
@@ -3,23 +3,27 @@ package com.EdumentumBackend.EdumentumBackend.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.FirebaseOptions.Builder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/google-services.json");
+        InputStream serviceAccount =
+                new ClassPathResource("google-services.json").getInputStream();
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setStorageBucket("project-64de8.appspot.com")
+        Builder builder = new Builder();
+        builder.setCredentials(GoogleCredentials.fromStream(serviceAccount));
+        builder.setStorageBucket("project-64de8.appspot.com");
+        FirebaseOptions options = builder
                 .build();
 
         return FirebaseApp.initializeApp(options);
