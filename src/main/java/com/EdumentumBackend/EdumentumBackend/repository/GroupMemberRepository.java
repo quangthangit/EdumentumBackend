bccda11e78f5b6f1a4188c4618ec699d68b5e2e0
quangthangit <thangtq.it@gmail.com>
2025-08-25 15:27:31 +0700

    fix: add anotation repository

--- src/main/java/com/EdumentumBackend/EdumentumBackend/repository/GroupMemberRepository.java
+++ src/main/java/com/EdumentumBackend/EdumentumBackend/repository/GroupMemberRepository.java
@@ -7,9 +7,11 @@ import com.EdumentumBackend.EdumentumBackend.entity.UserEntity;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface GroupMemberRepository extends JpaRepository<GroupMemberEntity, Long> {
     boolean existsByGroupAndUser(GroupEntity group, UserEntity user);
 
